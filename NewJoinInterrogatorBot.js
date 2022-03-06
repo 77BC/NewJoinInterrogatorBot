b9665a3d797909635f6cab86fadcb5e3f6216ba2
77BC <100898150+77BC@users.noreply.github.com>
2022-03-05 16:46:32 -0800

    Update NewJoinInterrogatorBot.js

--- NewJoinInterrogatorBot.js
+++ NewJoinInterrogatorBot.js
@@ -73,12 +73,12 @@ bot.on('guildMemberAdd', member => {
     memberToRandomChoices[member.id] = shuffleArray(memberToRandomQuestionAnswerEntry[member.id][1].split(" "));
     const embeded = new MessageEmbed()
                     .setColor('#00ffff')
-                    .setDescription(memberToRandomQuestionAnswerEntry[member.id][0] + " 请回复A/B/C/D" + "\r\n" 
+                    .setDescription(memberToRandomQuestionAnswerEntry[member.id][0] + "   请回复A/B/C/D" + "\r\n" 
                     + "A.  " + memberToRandomChoices[member.id][0] + "\r\n"
                     + "B.  " + memberToRandomChoices[member.id][1] + "\r\n"
                     + "C.  " + memberToRandomChoices[member.id][2] + "\r\n"
                     + "D.  " + memberToRandomChoices[member.id][3]);
-    console.log(memberToSent[member.id]);
+    console.log(memberToSent);
     if(memberToSent[member.id] == undefined) {
         member.send({ embeds: [embeded] });
         memberToSent[member.id] = true;
